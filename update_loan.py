1e65080396525706d94ccbf71c09fe729ffb4e77
beening01 <hanbeen0221@gmail.com>
2025-08-06 14:29:03 +0900

    fix:update_readme

--- update_loan.py
+++ update_loan.py
@@ -11,25 +11,27 @@ def update_readme():
             else:
                 df2 = pd.read_excel(xlsx, sheet_name=sheet_name)
 
-    md_table = "|날짜|ITEM_NAME1|DATA_VALUE|\n|---|---|---|\n"
+    md_table1 = "|날짜|ITEM_NAME1|DATA_VALUE|\n|---|---|---|\n"
     for _, row in df1.iterrows():
-        md_table += f"|{row['TIME']}|{row['ITEM_NAME1']}|{row['DATA_VALUE']}|\n"
+        md_table1 += f"|{row['TIME']}|{row['ITEM_NAME1']}|{row['DATA_VALUE']}|\n"
 
-    with open("README.md", "w", encoding="utf-8") as f:
-        f.write("# 📈 최근 1년간 주택담보대출 금리 흐름\n\n")
-        f.write("![주택담보대출](OUT_DIR/주택담보대출.png)\n\n")
-        f.write("## 📋 월별 금리 데이터\n\n")
-        f.write(md_table)
-
-        md_table = "|날짜|ITEM_NAME1|DATA_VALUE|\n|---|---|---|\n"
+    md_table2 = "|날짜|ITEM_NAME1|DATA_VALUE|\n|---|---|---|\n"
     for _, row in df2.iterrows():
-        md_table += f"|{row['TIME']}|{row['ITEM_NAME1']}|{row['DATA_VALUE']}|\n"
+        md_table2 += f"|{row['TIME']}|{row['ITEM_NAME1']}|{row['DATA_VALUE']}|\n"
 
     with open("README.md", "w", encoding="utf-8") as f:
-        f.write("# 📈 최근 1년간 일반신용대출 금리 흐름\n\n")
+        f.write("# 📈 최근 1년간 대출 금리 흐름\n\n")
+
+        f.write("## 🏠 주택담보대출\n\n")
+        f.write("![주택담보대출](OUT_DIR/주택담보대출.png)\n\n")
+        f.write("### 📋 월별 금리 데이터\n\n")
+        f.write(md_table1 + "\n\n")
+
+        f.write("## 💳 일반신용대출\n\n")
         f.write("![일반신용대출](OUT_DIR/일반신용대출.png)\n\n")
-        f.write("## 📋 월별 금리 데이터\n\n")
-        f.write(md_table)
+        f.write("### 📋 월별 금리 데이터\n\n")
+        f.write(md_table2 + "\n")
+
 
 if __name__ == "__main__":
     update_readme()
